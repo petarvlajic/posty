017f29b01c1a305b24e7286665b96689371e0eac
petarvlajicS3219 <vlajic.p27@gmail.com>
2021-07-26 21:53:50 +0200

    First commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
